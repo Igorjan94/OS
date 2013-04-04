340bb1952bca638684c59cb3e745989c7e9ed54d
Igor Kolobov <igorjan94@mail.ru>
2013-04-04 19:37:56 +0400

    try read

--- dupme/main.c
+++ dupme/main.c
@@ -15,12 +15,31 @@ int stringToInt(char* c)
     return ans;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     printf("%s is working normally\n", argv[0]);
     if (argc != 2)
     {
         return 1;
+    }
+    int k = stringToInt(argv[1]);
+    if (k < 1)
+	return 2;
+    char* buffer = (char* ) malloc(k);
+    int length, n;
+    int i, j;
+    length = 0;
+    while (1)
+    {
+	n = read(0, buffer + length, k - length);
+	if (n == 0)
+	    break;
+	if (n == -1)
+	{
+	    free(buffer);
+	    return 3;
+	}
+	break;
     }
     printf("%d\n", argc);
     return 0;
