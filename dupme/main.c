2272c1f5faa77b75906aa2154ed38557af59ae63
Igor Kolobov <igorjan94@mail.ru>
2013-04-04 17:28:25 +0400

    fix mistakes

--- dupme/main.c
+++ dupme/main.c
@@ -8,17 +8,20 @@ int stringToInt(char* c)
     while (*c) 
     {
         if (*c >= '0' && *c <= '9')
-	    ans = ans * 10 + *s - '0'; else
+	    ans = ans * 10 + *c - '0'; else
 	    return -1;
-	s++;
+	c++;
     }
     return ans;
 }
 
 int main(int argc, char **argv)
 {
+    printf("%s is working normally\n", argv[0]);
     if (argc != 2)
     {
         return 1;
     }
+    printf("%d\n", argc);
+    return 0;
 }
