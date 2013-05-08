7f48e33a1942c32d816b3116bef34c7938f1f305
Igor Kolobov <igorjan94@mail.ru>
2013-05-09 01:30:27 +0400

    brokensumlinks.c added forgotten check of /

--- simple-bash/brokensymlinks.c
+++ simple-bash/brokensymlinks.c
@@ -18,15 +18,22 @@ int brokenSymLinks(char* name)
     int length = strlen(name);
     char* buf = malloc(length + 512);
     memcpy(buf, name, sizeof(char) * (length + 1));
+    //printf("directory for search : %s\n", buf);
+    if (buf[length - 1] != '/')
+    {
+        buf[length] = '/';
+        buf[++length] = 0;
+    }
     while ((f = readdir(d)) != NULL)
     {
         if (!strcmp(f->d_name, ".") || !strcmp(f->d_name, ".."))
             continue;
-        memcpy(buf + length, f->d_name, sizeof(char)  * strlen(f->d_name));
+        memcpy(buf + length, f->d_name, sizeof(char) * (strlen(f->d_name) + 1));
         if (f->d_type == DT_DIR)
             brokenSymLinks(buf);
         if (f->d_type == DT_LNK && access(buf, F_OK) == -1)
             printf("broken symlink: %s\n", buf);
+        buf[length] = 0;
     }
     closedir(d);
     free(buf);
