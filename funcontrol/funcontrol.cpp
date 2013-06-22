d290636d51d229390defc8df65c17c851b51a053
Igor Kolobov <igorjan94@mail.ru>
2013-06-22 15:08:58 +0400

    added deamon

--- funcontrol/funcontrol.cpp
+++ funcontrol/funcontrol.cpp
@@ -7,37 +7,38 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define COUNT_OF_TEMPERATURES 4
 
 using namespace std;
 
 pid_t pid;
-
-void handler(int) {
-    kill(pid, SIGINT);
-}
 
 int main(int, char **)
 {
     pid = fork();
     if (pid)
     {
-        signal(SIGINT, handler);
-        std::cout << "Started daemon, pid: " << pid << std::endl;
+        cout << "Started daemon, pid: " << pid << "\n";
         int status;
         waitpid(pid, &status, 0);
-        std::cout << "Daemon stopped" << std::endl;
+        cout << "Daemon stopped" << "\n";
     } else
     {
-        int log_fd = dup(STDOUT_FILENO);
-        if (log_fd == -1)
-        {
-            _exit(43);
-        }
-
+        if (dup(STDOUT_FILENO) == -1)
+            _exit(1);
         close(STDIN_FILENO);
         close(STDOUT_FILENO);
         close(STDERR_FILENO);
         setsid();
+        int in = open("input.txt", O_RDONLY);
+        int out = open("output.txt", O_CREAT | O_WRONLY, 0644);
+        if (in < 0 || out < 0)
+        {
+            printf("fail opening file\n");
+            return 1;
+        }
+        close(in);
+        close(out);
     }
     return 0;
 }
